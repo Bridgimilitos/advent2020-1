92b408b25bfae7057b062808969aa6969f2aceda
Bridgimilitos <jbridges@f2s.com>
2021-05-23 18:48:57 +0100

    4 part 2

--- src/main/java/four/PassportValidator.java
+++ src/main/java/four/PassportValidator.java
@@ -44,17 +44,97 @@ public class PassportValidator
                    .collect(Collectors.toList());
     }
 
-    static void main(String[] args)
+    public static void main(String[] args)
     {
         try
         {
             System.out.println(PassportValidator.parseBatch("resource/input4").stream()
                 .filter(PassportValidator::validate)
+                .count()); 
+            
+            System.out.println(PassportValidator.parseBatch("resource/input4").stream()
+                .filter(PassportValidator::validatePart2)
                 .count());
         }
         catch (IOException e)
         {
             e.printStackTrace();
         }
+    }
+
+    static boolean validatePart2(Map<String, String> passport)
+    {
+        if (!validate(passport))
+        {
+            return false;
+        }
+
+        return validateBirthYear(passport.get("byr"))
+            && validateIssueYear(passport.get("iyr"))
+            && validateExpirationYear(passport.get("eyr")) 
+            && validateHeight(passport.get("hgt"))
+            && validateHairColor(passport.get("hcl"))
+            && validateEyeColor(passport.get("ecl"))
+            && validatePassportId(passport.get("pid"));
+    }
+
+    private static boolean validatePassportId(String pid)
+    {
+        //        pid (Passport ID) - a nine-digit number, including leading zeroes.
+        return pid.chars().filter(Character::isDigit).count() == 9;
+    }
+
+    private static boolean validateEyeColor(String ecl)
+    {
+        //        ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+        return List.of("amb","blu","brn","gry","grn","hzl","oth").contains(ecl);
+    }
+
+    private static boolean validateHairColor(String hcl)
+    {
+        //        hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+        return hcl.matches("#([0-9a-f]{6})");
+    }
+
+    private static boolean validateHeight(String hgt)
+    {
+        //        hgt (Height) - a number followed by either cm or in:
+        //          If cm, the number must be at least 150 and at most 193.
+        //          If in, the number must be at least 59 and at most 76.
+        if (hgt.endsWith("cm"))
+        {
+            int cm = Integer.parseInt(hgt.replaceAll("cm", ""));
+            return cm >= 150 && cm <= 193;
+        }
+        else if (hgt.endsWith("in"))
+        {
+            int in = Integer.parseInt(hgt.replaceAll("in", ""));
+            return in >= 59 && in <= 76;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private static boolean validateExpirationYear(String eyr)
+    {
+        //        eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+        int year = Integer.parseInt(eyr);
+        return year >= 2020 && year <= 2030;
+    }
+
+    private static boolean validateIssueYear(String iyr)
+    {
+        //        iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+        int year = Integer.parseInt(iyr);
+        return year >= 2010 && year <= 2020;
+    }
+
+    private static boolean validateBirthYear(String byr)
+    {
+        //        byr (Birth Year) - four digits; at least 1920 and at most 2002.
+        int year = Integer.parseInt(byr);
+        return year >= 1920 && year <= 2002;
     }
 }
--- src/test/java/four/PassportValidatorTest.java
+++ src/test/java/four/PassportValidatorTest.java
@@ -19,17 +19,27 @@ public class PassportValidatorTest
                                                         "cid", "147",
                                                         "hgt", "183cm");
 
+    private final Map<String, String> passport2 = Map.of("ecl", "grn",
+                                                        "pid", "087499704",
+                                                        "eyr", "2030",
+                                                        "hcl", "#623a2f",
+                                                        "byr", "1980",
+                                                        "iyr", "2012",
+                                                        "hgt", "74in");
+
     @Test
     void passportHasValidFields()
     {
-//        byr (Birth Year)
-//        iyr (Issue Year)
-//        eyr (Expiration Year)
-//        hgt (Height)
-//        hcl (Hair Color)
-//        ecl (Eye Color)
-//        pid (Passport ID)
-//        cid (Country ID) Optional - no point in checking.
+//        byr (Birth Year) - four digits; at least 1920 and at most 2002.
+//        iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+//        eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+//        hgt (Height) - a number followed by either cm or in:
+//          If cm, the number must be at least 150 and at most 193.
+//          If in, the number must be at least 59 and at most 76.
+//        hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+//        ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+//        pid (Passport ID) - a nine-digit number, including leading zeroes.
+//        cid (Country ID) - ignored, missing or not.
 
         //arrange
         //ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm
@@ -56,5 +66,33 @@ public class PassportValidatorTest
         assertEquals(2, PassportValidator.parseBatch("resource/input4test").stream()
             .filter(PassportValidator::validate)
             .count());
+    }
+
+    @Test
+    void countValidPassportsPart2() throws IOException
+    {
+        assertEquals(4, PassportValidator.parseBatch("resource/input4test2").stream()
+                            .filter(PassportValidator::validatePart2)
+                            .count());
+    }
+
+    @Test
+    void passportHasValidFieldsPart2()
+    {
+//        byr (Birth Year) - four digits; at least 1920 and at most 2002.
+//        iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+//        eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+//        hgt (Height) - a number followed by either cm or in:
+//          If cm, the number must be at least 150 and at most 193.
+//          If in, the number must be at least 59 and at most 76.
+//        hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+//        ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+//        pid (Passport ID) - a nine-digit number, including leading zeroes.
+//        cid (Country ID) - ignored, missing or not.
+
+        //arrange
+        //ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm
+        //act/assert
+        assertTrue(PassportValidator.validatePart2(passport));
     }
 }
